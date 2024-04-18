2c028b2040731aead923ab5224aed03a00242093
marcosCoelhoo <mparav124@gmail.com>
2024-04-18 09:12:28 -0300

    create adverts section and cards

---
+++ src/components/Home/Adverts/Adverts.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Adverts.module.css';
+import AdvertsCard from '../AdvertsCard/AdvertsCard';
+import dataCards from '../../../../data.json';
+
+const Adverts = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.adverts}>
+        <div className={styles.header}>
+          <h1 className={styles.headerTitle}>Anúncios Recentes</h1>
+
+          <ul className={styles.listCards}>
+            {dataCards.map((info, index) => (
+              <AdvertsCard key={index} data={info} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Adverts;
---
+++ src/components/Home/AdvertsCard/AdvertsCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './AdvertsCard.module.css';
+
+const AdvertsCard = ({ data }) => {
+  return (
+    <li className={styles.card}>
+      <div className={styles.image}>
+        <img
+          src="https://fakeimg.pl/352x200/863cff/fcfcfc?text=anunciante&font=lobster"
+          alt=""
+          loading="lazy"
+        />
+      </div>
+
+      <div className={styles.cardContent}>
+        <div className={styles.content}>
+          <h2>{data.title}</h2>
+          <p>{data.description}</p>
+        </div>
+
+        <div className={styles.info}>
+          <div className={styles.infoData}>
+            <i className="ph-bold ph-phone"></i>
+            <p>{data.phone}</p>
+          </div>
+
+          <div className={styles.infoData}>
+            <i className="ph-bold ph-map-pin"></i>
+            <p>{data.address}</p>
+          </div>
+        </div>
+
+        <div className={styles.buttons}>
+          <a href="#" className={`${styles.btnWhatsapp} ${styles.button}`}>
+            <i class="ph ph-whatsapp-logo"></i>
+            <p>WhatsApp</p>
+          </a>
+
+          <a href="#" className={`${styles.btnMoreInfo} ${styles.button}`}>
+            <p>Mais informações</p>
+          </a>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default AdvertsCard;
